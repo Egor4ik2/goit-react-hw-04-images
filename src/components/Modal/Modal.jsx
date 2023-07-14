bc05245336d5264b70e048771b20cd6e09403052
Egor Krechuniak <egorkrecunak@gmail.com>
2023-07-14 11:26:28 +0300

    fix

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,35 +1,31 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Modal.module.css';
 
 const Modal = ({ imageURL, onCloseModal }) => {
-  const modalRef = useRef(null);
-
   useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (modalRef.current && !modalRef.current.contains(event.target)) {
-        onCloseModal();
-      }
-    };
-
     const handleKeyDown = (event) => {
       if (event.code === 'Escape') {
         onCloseModal();
       }
     };
 
-    document.addEventListener('mousedown', handleClickOutside);
     document.addEventListener('keydown', handleKeyDown);
 
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
       document.removeEventListener('keydown', handleKeyDown);
     };
   }, [onCloseModal]);
 
+  const handleClick = (event) => {
+    if (event.target === event.currentTarget) {
+      onCloseModal();
+    }
+  };
+
   return (
-    <div className={styles.Modal} onClick={onCloseModal}>
-      <div ref={modalRef} className={styles.Overlay}>
+    <div className={styles.Modal} onClick={handleClick}>
+      <div className={styles.Overlay}>
         <img src={imageURL} alt="" />
       </div>
     </div>
